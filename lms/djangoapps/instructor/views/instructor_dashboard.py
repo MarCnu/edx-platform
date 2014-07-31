97925f4667e4a5c063120c0e1c0bb687e0932f40
Clinton Blackburn <cblackburn@edx.org>
2014-07-31 17:22:43 -0400

    Added link from instructor dashboard to analytics dashboard

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -103,12 +103,19 @@ def instructor_dashboard_2(request, course_id):
     if max_enrollment_for_buttons is not None:
         disable_buttons = enrollment_count > max_enrollment_for_buttons
 
+    deprecated_course_key = unicode(course_key)
+
+    analytics_dashboard_url = None
+    if settings.ANALYTICS_DASHBOARD_URL:
+        analytics_dashboard_url = '{0}/courses/{1}'.format(settings.ANALYTICS_DASHBOARD_URL, deprecated_course_key)
+
     context = {
         'course': course,
-        'old_dashboard_url': reverse('instructor_dashboard_legacy', kwargs={'course_id': course_key.to_deprecated_string()}),
+        'old_dashboard_url': reverse('instructor_dashboard_legacy', kwargs={'course_id': deprecated_course_key}),
         'studio_url': studio_url,
         'sections': sections,
         'disable_buttons': disable_buttons,
+        'analytics_dashboard_url': analytics_dashboard_url
     }
 
     return render_to_response('instructor/instructor_dashboard_2/instructor_dashboard_2.html', context)
